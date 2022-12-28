1ae03054ae79f1275b2c6ea44176a8d1e459a6ed
Giovanni Medeiros <ggabriel.medeiros@hotmail.com>
2022-12-28 20:40:29 -0300

    Formatação do código

--- script.js
+++ script.js
@@ -1,15 +1,13 @@
 async function getMensage(){
 
     try{
-    const api = await axios.get('https://api.adviceslip.com/advice')
-    const mensagem = api.data.slip.advice
-    console.log(mensagem)
-    let response = document.querySelector("#mensagem-camp")
-    response.innerHTML = ""
-    console.log(response)
-    let teste = document.createElement("p")
-    teste.innerHTML = mensagem
-    response.appendChild(teste)
+        const api = await axios.get('https://api.adviceslip.com/advice')
+        const mensagem = api.data.slip.advice
+        let response = document.querySelector("#mensagem-camp")
+        response.innerHTML = ""
+        let teste = document.createElement("p")
+        teste.innerHTML = mensagem
+        response.appendChild(teste)
     }catch(error){
         console.log(error)
     }
